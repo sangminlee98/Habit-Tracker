f2c688c3ea25097bf60e22158c9ab35b85c3fcf9
sangminlee98 <tkdals0975@naver.com>
2021-09-30 01:13:24 +0900

    + - delete 버튼 기능 구현

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
 
 class Habit extends Component {
-    state = {
-        count: 0,
-    }
-
     handleIncrement = () => {
-        this.setState({count: this.state.count+1});
-    }
+        this.props.onIncrement(this.props.habit);
+    };
     handleDecrement = () => {
-        const count = this.state.count-1;
-        this.setState({count: count<0 ? 0 : count});
-    }
+        this.props.onDecrement(this.props.habit);
+    };
+    handleDelete = () => {
+        this.props.onDelete(this.props.habit);
+    };
     render() {
         const {name,count} = this.props.habit;
         return (
@@ -24,7 +22,7 @@ class Habit extends Component {
                 <button className='habit-button habit-decrease' onClick={this.handleDecrement}>
                     <i className="fas fa-minus-square"></i>
                 </button>
-                <button className='habit-button habit-delete'>
+                <button className='habit-button habit-delete' onClick={this.handleDelete}>
                     <i className="fas fa-trash"></i>
                 </button>
             </li>
