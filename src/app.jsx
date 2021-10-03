56169770880582aeecc4ecac5afc9e0286ad5720
sangminlee98 <tkdals0975@naver.com>
2021-10-03 13:39:32 +0900

    PureComponenet 적용

--- src/app.jsx
+++ src/app.jsx
@@ -14,17 +14,23 @@ class App extends Component {
   };
 
   handleIncrement = (habit) => {
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    habits[index].count++;
+    const habits = this.state.habits.map(item => {
+      if(item.id === habit.id) {
+        return {...habit,count:item.count+1};
+      };
+      return item;
+    })
     this.setState({habits});
     
   };
   handleDecrement = (habit) => {
-      const habits = [...this.state.habits];
-      const index = habits.indexOf(habit);
-      const count = habits[index].count-1;
-      habits[index].count = count<0 ? 0 : count;
+      const habits = this.state.habits.map(item => {
+        if(item.id === habit.id) {
+          const count = habit.count-1;
+          return {...habit,count: count < 0 ? 0 : count};
+        }
+        return item;
+      })
       this.setState({habits});
   };
   handleDelete = (habit) => {
@@ -42,9 +48,11 @@ class App extends Component {
     }
 
   handleReset = () => {
-    const habits = [...this.state.habits];
-    habits.map( habit => {
-      habit.count = 0;
+    const habits = this.state.habits.map(item => {
+      if(item.count !== 0) {
+        return {...item,count:0};
+      }
+      return item;
     });
     this.setState({habits});
   }
