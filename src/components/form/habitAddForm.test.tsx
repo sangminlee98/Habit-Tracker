36217e3b4c0faffc781d393903d1ab442aa304b6
sangminlee98 <tkdals0975@naver.com>
2022-11-16 10:12:44 +0900

    test : habitAddForm userEvent v14
    userEvent 14버전부터는 render 전에 setup()을 실행하고
    async await으로 user.click, user.type 등 사용해야 한다

--- src/components/form/habitAddForm.test.tsx
+++ src/components/form/habitAddForm.test.tsx
@@ -3,6 +3,7 @@ import userEvent from "@testing-library/user-event";
 import React from "react";
 import HabitAddForm from "./habitAddForm";
 import renderer from "react-test-renderer";
+import { UserEvent } from "@testing-library/user-event/dist/types/setup/setup";
 
 describe("HabitAddForm", () => {
   it("renders", () => {
@@ -14,7 +15,9 @@ describe("HabitAddForm", () => {
     let onAdd: (name: string) => void;
     let input: HTMLElement;
     let button: HTMLElement;
+    let user: UserEvent;
     const setup = () => {
+      user = userEvent.setup();
       render(<HabitAddForm onAdd={onAdd} />);
       input = screen.getByPlaceholderText("Habit");
       button = screen.getByText("Add");
@@ -22,20 +25,20 @@ describe("HabitAddForm", () => {
     beforeEach(() => {
       onAdd = jest.fn();
     });
-    it("calls onAdd when button is clicked", () => {
+    it("calls onAdd when button is clicked", async () => {
       setup();
       // input에다가 원하는 습관의 이름을 타이핑 한 다음에
       // add라는 버튼을 클릭하면
       // onAdd가 input에 입력된 이름과 함께 호출되어야 함
-      userEvent.type(input, "New Habit");
-      userEvent.click(button);
+      await user.type(input, "New Habit");
+      await user.click(button);
       expect(onAdd).toHaveBeenCalledTimes(1);
       expect(onAdd).toHaveBeenCalledWith("New Habit");
     });
     it("does not call onAdd when the habit is empty", () => {
       setup();
-      userEvent.clear(input);
-      userEvent.click(button);
+      user.clear(input);
+      user.click(button);
       expect(onAdd).toHaveBeenCalledTimes(0);
     });
   });
